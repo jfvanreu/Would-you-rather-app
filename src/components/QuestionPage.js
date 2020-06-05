3bc48e6ff471b90755e4321221a6a4008b944bc7
jfvanreu <jfvanreu@comcast.net>
2020-06-05 10:36:11 -0700

    add 404 for invalid question Ids

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QuestionAnswer from './QuestionAnswer'
 import QuestionResults from './QuestionResults'
+import PageNotFound from './PageNotFound'
 
 class QuestionPage extends Component {
 
     render() {
-        const { questionId, answeredQuestion } = this.props
+        const { questionId, answeredQuestion, foundQuestion } = this.props
         
-        return (answeredQuestion ?  <QuestionResults questionId={questionId}/> : <QuestionAnswer questionId={questionId}/>)  
+        if (!foundQuestion) {
+            return <PageNotFound />
+        } else {
+            return (answeredQuestion ?  <QuestionResults questionId={questionId}/> : <QuestionAnswer questionId={questionId}/>)
+        }
     }
 }
 
 function mapStateToProps({authedUser, questions, users}, props) {
     const questionId = props.match.params.QuestionId
     const question = questions[questionId]
-    const answeredQuestion = (question.optionOne.votes.includes(authedUser)) ||  (question.optionTwo.votes.includes(authedUser))
+    const foundQuestion = question ? true : false
+    const answeredQuestion = foundQuestion ? ((question.optionOne.votes.includes(authedUser)) ||  (question.optionTwo.votes.includes(authedUser))) : (false)
 
     return {
         questionId,
         answeredQuestion,
+        foundQuestion,
     }
 }
 
